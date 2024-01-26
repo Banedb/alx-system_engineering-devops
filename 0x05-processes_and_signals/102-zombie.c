431abcd6976a25644b4504b367cc64d06e5bb337
Bane <marcytornad@gmail.com>
2024-01-26 14:50:18 -0800

    102

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,2 +1,45 @@
-#!/usr/bin/env bash
-# bash script to
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * infinite_while - Creates an infinite loop for the main process.
+ * Return: Always 0.
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - Creates 5 zombie processes.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	pid_t child_pid;
+	int i;
+
+	for (i = 0; i < 5; i++)
+	{
+		child_pid = fork();
+
+		if (child_pid == -1)
+		{
+			perror("fork");
+			exit(EXIT_FAILURE);
+		}
+
+		if (child_pid == 0)
+		{
+			printf("Zombie process created, PID: %d\n", getpid());
+			exit(0);
+		}
+	}
+	infinite_while();  /* Keeps the main process running */
+	return (0);
+}
