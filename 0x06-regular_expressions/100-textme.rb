b1a687877019fc4fcb55275dd8258aa3967f50c1
Bane <marcytornad@gmail.com>
2024-02-02 08:51:13 -0800

    100

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,3 @@
 #!/usr/bin/env ruby
 puts ARGV[0].match(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).captures.join(',')
+# puts ARGV[0].scan(/\[from:(.+?)\] \[to:(\+?\d+)\] \[flags:(.+?)\]/).join(',')
