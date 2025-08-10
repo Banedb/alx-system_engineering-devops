724793c6ca64f15903f362cb077c4a60c060badd
Bane <marcytornad@gmail.com>
2025-08-10 18:29:36 +0100

    all

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# Creates a file `/tmp/school` using Puppet.
+file { '/tmp/school':
+  ensure  => 'present',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
