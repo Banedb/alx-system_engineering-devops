acd5f32db25a5c64dd0184bdb44c57bdfbc4aab5
Bane <121007715+Banedb@users.noreply.github.com>
2025-08-10 18:45:49 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,3 +3,9 @@ package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3'
 }
+# Fix for Check failing because of incompatible werkzeug version.
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['flask'],
+}
